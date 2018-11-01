8749987ba755a07192577fd049da83d7657eea40
knyghtlyght <kevnaf@gmail.com>
2018-11-01 10:10:53 -0700

    catchup10-31

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="item-card">
+    <span class="item-level">{{item.level}}}</span>
+    <span class="search-level">{{}}</span>
+    <h3 class="item-name">{{item.name}}</h3>
+    <h4 class="item-class">{{item.class}}</h4>
+    <span class="item-base">{{item.base}}</span>
+    <span class="item-rarity">{{item.rarity}}</span>
+    <ul class="item-effects">
+      <li v-for="effect in item.effects" :key="effect.id">{{effect.name}} {{effect.effect}}</li>
+    </ul>
+    <p class="item-flavor">{{item.flavor}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: Object
+  },
+  data() {}
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/ItemQuantityGen.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="quantity-card">
+    <form @submit.prevent="generateItemTypes" class="quantity-search">
+      <div>
+        <label>Party Level</label>
+        <input v-model="quantityList.partyLevel" type="number" step="1">
+      </div>
+      <div>
+        <label>Minions</label>
+        <input v-model="monsterList.minion" type="number" step="1">
+      </div>
+      <div>
+        <label>Elites</label>
+        <input v-model="monsterList.elite" type="number" step="1">
+      </div>
+      <div>
+        <label>Champions</label>
+        <input v-model="monsterList.champ" type="number" step="1">
+      </div>
+      <div>
+        <label>Bosses</label>
+        <input v-model="monsterList.boss" type="number" step="1">
+      </div>
+      <input type="submit" value="Submit">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quantityList: this.createFreshQuantityList(),
+      monsterList: this.createFreshMonsterList()
+    };
+  },
+  methods: {
+    createFreshQuantityList() {
+      return {
+        partyLevel: 0,
+        none: 0,
+        common: 0,
+        uncommon: 0,
+        rare: 0,
+        vRare: 0,
+        legendary: 0,
+        artifact: 0
+      };
+    },
+    createFreshMonsterList() {
+      return {
+        minion: 0,
+        elite: 0,
+        champ: 0,
+        boss: 0
+      };
+    },
+    generateItemTypes() {
+      var rarityList = [
+        "none",
+        "common",
+        "uncommon",
+        "rare",
+        "vRare",
+        "legendary",
+        "artifact"
+      ];
+      var minionWeight = [0.8, 0.1, 0.05, 0.05, 0, 0, 0];
+      for (var i = 0; i < this.monsterList.minion; i++) {
+        ++this.quantityList[this.generateRarity(rarityList, minionWeight)];
+      }
+    },
+    generateRarity(list, weight) {
+      var random_num = Math.random() * (1.0 - 0.0) + 0.0;
+      var weight_sum = 0;
+
+      for (var i = 0; i < list.length; i++) {
+        weight_sum += weight[i];
+        weight_sum = +weight_sum.toFixed(3);
+
+        if (random_num <= weight_sum) {
+          return list[i];
+        }
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
